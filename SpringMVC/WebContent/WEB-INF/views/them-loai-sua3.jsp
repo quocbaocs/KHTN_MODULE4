f4ebba6459070683abc32d437dc73c4812384d22
quocbaole <quocbaole368@gmail.com>
2022-11-11 19:19:48 +0700

    pathvariable, cookie, request param spring

--- SpringMVC/WebContent/WEB-INF/views/them-loai-sua3.jsp
+++ SpringMVC/WebContent/WEB-INF/views/them-loai-sua3.jsp
@@ -8,10 +8,10 @@
 <title>Thêm</title>
 </head>
 <body>
-	<form action="them-loai-sua2" method="post">
+	<form action="them-loai-sua3" method="post">
 		<h1>Thêm loại sữa</h1>
-		<p><label>Mã loại:</label><input type="text" name="txtMaLoai"></p>
-		<p><label>Tên loại:</label><input type="text" name="txtTenLoai"></p>
+		<p><label>Mã loại:</label><input type="text" name="maLoai"></p>
+		<p><label>Tên loại:</label><input type="text" name="tenLoai"></p>
 		<p><input type="submit" name="btnThem" value="Thêm"></p>
 	</form>
 	<c:if test="${not empty tb}">
--- SpringMVC/src/businessLogics/LoaiSuaBL.java
+++ SpringMVC/src/businessLogics/LoaiSuaBL.java
@@ -11,12 +11,12 @@ import java.util.List;
 import javaBeans.LoaiSua;
 
 public class LoaiSuaBL {
-	public static List<LoaiSua> docTatCa(){
+	public static List<LoaiSua> docTatCa() {
 		List<LoaiSua> dsls = new ArrayList<>();
-		try (Connection ketNoi = CSDL.getKetNoi()){
+		try (Connection ketNoi = CSDL.getKetNoi()) {
 			Statement stm = ketNoi.createStatement();
 			ResultSet rs = stm.executeQuery("select * from loai_sua");
-			while(rs.next()) {
+			while (rs.next()) {
 				LoaiSua ls = new LoaiSua();
 				ls.setMaLoai(rs.getString("ma_loai_sua"));
 				ls.setTenLoai(rs.getString("ten_loai"));
@@ -27,9 +27,29 @@ public class LoaiSuaBL {
 		}
 		return dsls;
 	}
+
+	public static LoaiSua docTheoMaLoai(String maLoai) {
+		LoaiSua ls = null;
+		try (Connection ketNoi = CSDL.getKetNoi()) {
+			Statement stm = ketNoi.createStatement();
+			ResultSet rs = stm.executeQuery("select * from loai_sua where ma_loai_sua='" + maLoai+"'");
+			if (rs.next()) {
+				ls = new LoaiSua();
+				ls.setMaLoai(rs.getString("ma_loai_sua"));
+				ls.setTenLoai(rs.getString("ten_loai"));
+
+			}
+			return ls;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+
 	public static int them(LoaiSua ls) {
 		String sql = "insert into loai_sua(ma_loai_sua, ten_loai) values (?,?)";
-		try (Connection ketNoi = CSDL.getKetNoi()){
+		try (Connection ketNoi = CSDL.getKetNoi()) {
 			PreparedStatement pst = ketNoi.prepareStatement(sql);
 			pst.setString(1, ls.getMaLoai());
 			pst.setString(2, ls.getTenLoai());
@@ -39,14 +59,15 @@ public class LoaiSuaBL {
 			return 0;
 		}
 	}
+
 	public static LoaiSua tim(String maLoai) {
 		LoaiSua ls = null;
 		String sql = "select * from loai_sua where ma_loai_sua=?";
-		try (Connection ketNoi = CSDL.getKetNoi()){
+		try (Connection ketNoi = CSDL.getKetNoi()) {
 			PreparedStatement pst = ketNoi.prepareStatement(sql);
-			pst.setString(1,maLoai);
+			pst.setString(1, maLoai);
 			ResultSet rs = pst.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				ls = new LoaiSua();
 				ls.setMaLoai(rs.getString("ma_loai_sua"));
 				ls.setTenLoai(rs.getString("ten_loai"));
@@ -56,28 +77,33 @@ public class LoaiSuaBL {
 		}
 		return ls;
 	}
+
 	public static int sua(LoaiSua ls) {
 		String sql = "update loai_sua set ten_loai = ? where ma_loai_sua=?";
-		try (Connection ketNoi = CSDL.getKetNoi()){
+		try (Connection ketNoi = CSDL.getKetNoi()) {
 			PreparedStatement pst = ketNoi.prepareStatement(sql);
-			pst.setString(1,ls.getTenLoai());
-			pst.setString(2,ls.getMaLoai());
+			pst.setString(1, ls.getTenLoai());
+			pst.setString(2, ls.getMaLoai());
 			return pst.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return 0;
 		}
 	}
+
 	public static int xoa(String maLoai) {
 		String sql = "delete from loai_sua where ma_loai_sua=?";
-		try (Connection ketNoi = CSDL.getKetNoi()){
+		try (Connection ketNoi = CSDL.getKetNoi()) {
 			PreparedStatement pst = ketNoi.prepareStatement(sql);
-			pst.setString(1,maLoai);
+			pst.setString(1, maLoai);
 			return pst.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return 0;
 		}
 	}
-	
+	public static void main(String[] args) {
+		System.out.println(docTheoMaLoai("SD").getTenLoai());
+	}
+
 }
