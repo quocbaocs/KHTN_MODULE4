1d02c59d472557ed6c84c35909a30f649fe036d2
quocbaole <quocbaole368@gmail.com>
2022-11-26 09:43:26 +0700

    update menu

--- SpringForm/src/controllers/HomeController.java
+++ SpringForm/src/controllers/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import businessLogics.CategoryBL;
 import businessLogics.ProductBL;
 
 @Controller
@@ -11,6 +12,8 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public String home(Model model) {
+		model.addAttribute("children", CategoryBL.getChildren());
+		model.addAttribute("parents", CategoryBL.getParrents());
 		model.addAttribute("products", ProductBL.getProducts());
 		return "chao";
 	}
